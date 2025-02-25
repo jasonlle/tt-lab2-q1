e2177c9220a5ea9c58dc8fc9f0fe520e2c35feb8
jasonlle <jl14793@nyu.edu>
2025-02-24 20:12:17 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt-lab2-q1 user_project (
+  tt_lab2_q1 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
